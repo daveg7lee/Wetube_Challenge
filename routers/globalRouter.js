7a1f8a3a753e8c79522a454c7f3b2d268b4a2f43
dave <daveg7lee@gmail.com>
2020-10-10 16:15:56 +0900

    install pug and make layout

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -7,8 +7,8 @@ const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, logout);
-globalRouter.get(routes.logout, login);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
 
 export default globalRouter;
