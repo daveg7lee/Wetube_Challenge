895fbbb191a0814f779f7d71f9b3eef0e47f585b
dave <daveg7lee@gmail.com>
2020-10-11 18:18:49 +0900

    User Profile, Edit User Password, Change Password

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { changePassword, editProfile, userDetail } from "../controllers/userController";
+import { getChangePassword, getEditProfile, postEditProfile, userDetail } from "../controllers/userController";
 import routes from "../routes";
-import { onlyPrivate } from "../middlewares";
+import { onlyPrivate, uploadAvatar } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile,onlyPrivate, editProfile);
-userRouter.get(routes.changePassword,onlyPrivate, changePassword)
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar,postEditProfile)
+
+userRouter.get(routes.changePassword,onlyPrivate, getChangePassword)
+userRouter.post(routes.changePassword, onlyPrivate, postEditProfile)
+
 userRouter.get(routes.userDetail(), userDetail);
 
 
