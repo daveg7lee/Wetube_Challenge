34d4bc347f08efc11c36044ba68c92a35db13105
dave <daveg7lee@gmail.com>
2020-10-11 19:28:01 +0900

    Recording video part Two

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -5,24 +5,24 @@ const videoPreview = document.getElementById("jsVideoPreview");
 let streamObject;
 let videoRecorder;
 
-const handleVideoData = event => {
-    const { data: videoFile } = event;
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(videoFile);
-    link.download = "recorded.webm";
-    document.body.appendChild(link);
-    link.click();
+const handleVideoData = (event) => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
 };
 
 const stopRecording = () => {
-    videoRecorder.stop();
-    recordBtn.removeEventListener("click", stopRecording);
-    recordBtn.addEventListener("click", getVideo);
-    recordBtn.innerHTML = "Start recording";
-  };
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
+};
 
 const startRecording = () => {
-    videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder = new MediaRecorder(streamObject);
   videoRecorder.start();
   videoRecorder.addEventListener("dataavailable", handleVideoData);
   recordBtn.addEventListener("click", stopRecording);
@@ -32,7 +32,7 @@ const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
-      video: { width: 1280, height: 720 }
+      video: { width: 1280, height: 720 },
     });
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
@@ -42,9 +42,9 @@ const getVideo = async () => {
     startRecording();
   } catch (error) {
     recordBtn.innerHTML = "☹️ Cant record";
-    recordBtn.removeEventListener("click", startRecording);
   } finally {
     recordBtn.removeEventListener("click", getVideo);
+  }
 };
 
 function init() {
