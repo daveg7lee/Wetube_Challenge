0444b19ee3633a007098aedbbf433ad687c711e2
dave <daveg7lee@gmail.com>
2020-10-17 20:57:52 +0900

    bug fix

--- src/db.js
+++ src/db.js
@@ -3,17 +3,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 mongoose.connect(
-  process.env.MONGO_URL,
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
   {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   }
 );
 
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅  Connected to DB");
-const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+const handleError = (error) =>
+  console.log(`❌ Error on DB Connection:${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError);
--- src/routes.js
+++ src/routes.js
@@ -21,7 +21,7 @@ const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
-const GITHUB = "/auth/github";
+const GITHUB = "auth/github";
 const GITHUB_CALLBACK = "/auth/github/callback";
 
 const API = "/api";
