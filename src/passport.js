13c343e9bc0aa82767c0717f80cbe57967f21b25
dave <daveg7lee@gmail.com>
2020-10-18 11:24:05 +0900

    Bug fix

--- src/passport.js
+++ src/passport.js
@@ -11,7 +11,9 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `https://polar-sea-27980.herokuapp.com${routes.githubCallback}`,
+      callbackURL: process.env.PRODUCTION
+        ? `https://floating-sea-99460.herokuapp.com/${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`,
     },
     githubLoginCallback
   )
